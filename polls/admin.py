44757ce33d13df33aba3c97507362d7fda7a9018
Jimmy Ha <herereadthis@gmail.com>
2014-08-14 00:34:54 -0400

    some edits to make admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from polls.models import Choice, Poll
 
 # Register your models here.
+# customize the admin form by creating a model admin object, then pass it
+# as the second argument to admin.site.register()
+# inline: you can do TabularInline (saves space) or StackedInline
+
+
+class ChoiceInline(admin.TabularInline):
+    # Choice Model
+    model = Choice
+    # add extra fields
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+            {
+                'fields': ['question']
+            }),
+        ('Date information',
+            {
+                'fields': ['pub_date'],
+                # makes the makes the fieldset auto-hide
+                'classes': ['collapse']
+            }),
+    ]
+    # tells Django: choice objects are edited on the Poll admin page. By
+    # default, provide enough field for 3 choices.
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+
+admin.site.register(Poll, PollAdmin)
